a73d7fd9830361b70efe0afc97404ee35a4d1ada
gyorgyjacint <jicant@gmail.com>
2024-05-30 13:44:15 +0200

    add: Styling

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -25,4 +25,25 @@ const styles = StyleSheet.create({
     height: 1,
     width: '80%',
   },
+camera: {
+    flex: 1,
+    width: '100%',
+    height: '100%'
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: 'transparent',
+    margin: 64,
+  },
+  button: {
+    flex: 1,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white',
+  },
 });
