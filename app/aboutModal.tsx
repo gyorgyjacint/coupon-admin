5fb3ac7908bc1c89fd11fa4a58c9f4238c70739a
gyorgyjacint <jicant@gmail.com>
2024-06-03 21:00:24 +0200

    add: About info

--- app/aboutModal.tsx
+++ app/aboutModal.tsx
@@ -1,19 +1,27 @@
 import { StatusBar } from "expo-status-bar";
 import { Platform, StyleSheet } from "react-native";
-
-import EditScreenInfo from "@/components/EditScreenInfo";
 import { Text, View } from "@/components/Themed";
+import data from "./../app.json";
 
 export default function AboutModalScreen() {
+  const appData = data?.expo;
+
+  if (!appData) {
+    return <View />
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>This is going to be an about page</Text>
+      <Text style={styles.title}>About</Text>
       <View
         style={styles.separator}
         lightColor="#eee"
         darkColor="rgba(255,255,255,0.1)"
       />
-      <EditScreenInfo path="app/modal.tsx" />
+      <Text style={styles.text}>Name: {appData.name}</Text>
+      <Text style={styles.text}>App version: {appData.version}</Text>
+      <Text style={styles.text}>Contact: random@e.mail</Text>
+      <Text style={styles.text}>Report a bug: random2@e.mail</Text>
 
       {/* Use a light status bar on iOS to account for the black space above the modal */}
       <StatusBar style={Platform.OS === "ios" ? "light" : "auto"} />
@@ -28,8 +36,11 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   title: {
-    fontSize: 20,
+    fontSize: 26,
     fontWeight: "bold",
+  },
+  text: {
+    fontSize: 18,
   },
   separator: {
     marginVertical: 30,
